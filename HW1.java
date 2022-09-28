a4bf2047112ac63882a7b13894d8b5504be9eccf
hanankaled161 <110997076+hanankaled161@users.noreply.github.com>
2022-09-28 21:46:51 +0300

    Add files via upload
    Signed-off-by: hanankaled161 <110997076+hanankaled161@users.noreply.github.com>

---
+++ HW1.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class HW1 {
+    public static void main(String[] args) {
+        System.out.println("السلام عليكم");
+
+        System.out.println("The sum of the first ten positive integers 1+2...+10 =");
+        System.out.print(1+2+3+4+5+6+7+8+9+10);
+
+        System.out.println("The product of the first ten positive integers 1+2...+10 =");
+        System.out.print(1*2*3*4*5*6*7*8*9*10);
+
+        System.out.println("+------+");
+        System.out.println("|hanan|");
+        System.out.println("+------+");
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.print(5 + 15 / 3 * 2 - 8 % 3);
+
+        System.out.println("   *");
+        System.out.println("  * *");
+        System.out.println(" *   *");
+        System.out.println("+-----+");
+        System.out.println("| .-. |");
+        System.out.println("| | | |");
+        System.out.println("+-+-+-+");
+
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+        JOptionPane.showMessageDialog(null, "Hello, HANAN!");
+
+    }
+}
